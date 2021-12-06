5677a0b6442d2cea0bee55ccef1bdb1aa36e2393
zhangyanming <zhangyanmingjiayou@163.com>
2021-12-06 18:17:51 +0800

    add  go / python file code

--- programming-language/code-demo/file/go-file.go
+++ programming-language/code-demo/file/go-file.go
@@ -1,10 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
-// main 
+/**
+ * 判断文件是否存在
+ * 存在返回 true 不存在返回false
+ */
+func IsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+// 读文件
+func ReadFile(fileName string) {
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	// 字节转数组
+	fmt.Println(string(content))
+}
+
+// 写文件
+func WriteFile(msg string, fileName string) {
+	if !IsExist(fileName) {
+		os.Create(fileName)
+	}
+	// 不能追加
+	err := ioutil.WriteFile(fileName, []byte(msg), fs.ModeAppend)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+// 追加写入
+func AppendFile(msg string, fileName string) {
+	if !IsExist(fileName) {
+		os.Create(fileName)
+	}
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	_, err2 := f.WriteString(msg)
+	if err2 != nil {
+		log.Fatal(err2.Error())
+	}
+}
+
+// 删除文件
+func RemoveFile(fileName string) {
+	err := os.Remove(fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("删除[%v]成功 \n", fileName)
+	}
+}
+
+// main
 // go run go-string.go
 func main() {
 	fmt.Println("start ...")
+	fileName := "go-test.log"
+	WriteFile("write test", fileName)
+	WriteFile("write test", fileName)
+	WriteFile("write test", fileName)
+	AppendFile("append test", fileName)
+	AppendFile("append test", fileName)
+	AppendFile("append test", fileName)
+	ReadFile(fileName)
+	RemoveFile(fileName)
 	fmt.Println("end ...")
 }
--- programming-language/code-demo/file/py-file.py
+++ programming-language/code-demo/file/py-file.py
@@ -1,7 +1,41 @@
 #!/usr/bin/env python3
 
+
+import os
+
+
+def writeToFile(msg, fileName):
+    """
+    写文件
+    """
+    # 写 w 写 w+ 读写 会删除原有内容  a 追加
+    with open(file=fileName, mode='a', encoding='utf-8') as f:
+        f.write(msg)
+
+
+def printFile(fileName):
+    """
+    按行读文件
+    """
+    # 读
+    with open(file=fileName, mode='r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line)
+
+
+def removeFile(fileName):
+    os.remove(fileName)
+
+
 def run():
     print("start ...")
+    file = "test.log"
+    writeToFile("test\n", fileName=file)
+    writeToFile("test", fileName=file)
+    writeToFile("test", fileName=file)
+    printFile(fileName=file)
+    removeFile(file)
     print("end ...")
 
 
