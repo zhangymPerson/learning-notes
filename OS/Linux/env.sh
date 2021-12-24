da0a3a3fb0f657b20a9656d48c98e2d9d9da08c9
zhangyanming <zhangyanmingjiayou@163.com>
2021-12-24 11:50:12 +0800

    alter alias conf

--- OS/Linux/env.sh
+++ OS/Linux/env.sh
@@ -3,6 +3,18 @@
 # 使用 source 命令使当前环境生效
 # 配置自己开发的环境变量
 
+# 查看系统变量 set
+
+# 当前路径 ${PWD}
+# 当前登录用户 ${LOGNAME}
+
+# alias 不支持参数，function 才支持。
+# 以快速运行 "grep --help" 为例, 有了以下alias运行“h grep”就可以了：
+alias h='help_fun(){ $@ --help | head -n 5 ;}; help_fun '
+
+# 查看文件前5行
+alias cat-file='aFun(){ cat $1 | head -n 5 ;}; aFun '
+
 # 获取host
 host=$(hostname -i)
 
@@ -27,13 +39,14 @@ alias tail-test='tail ${work}/log.log'
 alias mysql-test='mysql -h127.0.0.1 -uroot -p123456'
 
 # 文件上传服务
-alias nc-file='echo -e "客户端复制此命令\nfile为要上传的文件\nnc ${host} 8889 < file" && nc -l 8889 > file'
+alias nc-file='ncFunc(){ echo -e "客户端复制此命令 \nfile为要上传的文件 \nnc ${host} 8889 < $1" && nc -l 8889 > $1 ;}; ncFunc '
 
 # 文件下载服务
-alias download-file='echo "浏览器打开 http://${host}:8889/" && python3 -m http.server  8889'
+alias download-file='echo "浏览器打开 http://${HOSTNAME}:8889/" && python3 -m http.server  8889'
 
-# scp 等其他服务
-alias scp-file='scp file work@127.0.0.1:/home/work'
+# scp / ftp 等其他服务
+alias pwdftp='echo "ftp://${HOSTNAME}:${PWD}"'
+alias pwdscp='echo "${LOGNAME}@${HOSTNAME}:${PWD}"'
 
 echo "配置完成"
 # alias
