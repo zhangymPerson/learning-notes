f6af94fad49e133b2b61cd93bb84d8fd08840d0a
zhangyanming <zhangyanmingjiayou@163.com>
2022-12-07 14:43:04 +0800

    add python script  export swagger json file

--- internet/openapi/api_design.md
+++ internet/openapi/api_design.md
@@ -1,13 +1,22 @@
-# api接口设计
+# api 接口设计
 
 ## 接口参数设计
 
-## RESTful请求
+- 接口设计规范
 
+  [openapi](https://www.openapis.org/)
+
+  [openapi-中文规范](https://www.openapis.org/)
+
+  [openapi-github](https://github.com/OAI)
+
+  [OpenAPITools](https://github.com/OpenAPITools)
+
+## RESTful 请求
 
 ## 异常处理/错误码
 
-- 服务器向用户返回的状态码和提示信息，常见的有以下一些（方括号中是该状态码对应的HTTP动词）。
+- 服务器向用户返回的状态码和提示信息，常见的有以下一些（方括号中是该状态码对应的 HTTP 动词）。
 
 | 状态码 | 请求方式                               | 说明                                                                       |
 | ------ | -------------------------------------- | -------------------------------------------------------------------------- |
@@ -17,9 +26,9 @@
 | 204    | NO CONTENT - [DELETE]                  | 用户删除数据成功。                                                         |
 | 400    | INVALID REQUEST - [POST/PUT/PATCH]     | 用户发出的请求有错误，服务器没有进行新建或修改数据的操作，该操作是幂等的。 |
 | 401    | Unauthorized - [*]                     | 表示用户没有权限（令牌、用户名、密码错误）。                               |
-| 403    | Forbidden - [*]                        | 表示用户得到授权（与401错误相对），但是访问是被禁止的。                    |
+| 403    | Forbidden - [*]                        | 表示用户得到授权（与 401 错误相对），但是访问是被禁止的。                  |
 | 404    | NOT FOUND - [*]                        | 用户发出的请求针对的是不存在的记录，服务器没有进行操作，该操作是幂等的。   |
-| 406    | Not Acceptable - [GET]                 | 用户请求的格式不可得（比如用户请求JSON格式，但是只有XML格式）。            |
+| 406    | Not Acceptable - [GET]                 | 用户请求的格式不可得（比如用户请求 JSON 格式，但是只有 XML 格式）。        |
 | 410    | Gone -[GET]                            | 用户请求的资源被永久删除，且不会再得到的。                                 |
 | 422    | Unprocesable entity - [POST/PUT/PATCH] | 当创建一个对象时，发生一个验证错误。                                       |
 | 500    | INTERNAL SERVER ERROR - [*]            | 服务器发生错误，用户将无法判断发出的请求是否成功。                         |
---
+++ internet/openapi/py_getapi.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+@file : py_getapi.py
+@desc : 脚本运行方式 [python3 py_getapi.py]
+        脚本说明: 根据 swagger  地址 生成 json 文档
+@date : 2022-12-07 10:23:48
+@auth : danao
+@version : 1.0
+"""
+import requests
+import json
+import os
+import argparse
+
+
+def get_openapi_json(url, file_name):
+    payload = {}
+    headers = {}
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        obj = json.loads(response.text)
+        obj_json_str = json.dumps(
+            obj, ensure_ascii=False, default=str, indent=2)
+        # print(f"obj_json_str = [{obj_json_str}]")
+        with open(file=file_name, mode='a', encoding='utf-8') as f:
+            f.write(obj_json_str)
+            print(f"json 写入[{file_name}]文件成功!")
+    except Exception as e:
+        print(f"处理异常,e = [{e}]")
+
+
+def conf_args():
+    """函数的作用是:
+    获取用户指定参数输入
+    Returns:
+        return res 返回用户输入的参数字典
+    Raises:
+        列出与接口有关的所有异常.
+    """
+    name = os.path.basename(__file__)
+    args = argparse.ArgumentParser(
+        description=f'脚本[{name}]执行中的参数介绍', epilog='请按照以上说明执行脚本')
+    # 配置参数和说明
+    args.add_argument("-u", "--url", type=str,
+                      help="swagger 文档的 url 链接", default="http://127.0.0.1:8080/project_name/v3/api-docs")
+    args.add_argument("-f", "--file", type=str,
+                      help="导出json的文档地址", default="~/openapi.json")
+    args = args.parse_args()
+    # print(f"args = [{args}]")
+    # 使用方式  args.info 即可获取到参数值  如果有 dest 配置则读取dest 无则读取 -- 后面的属性配置
+    return args
+
+
+def main():
+    """
+    主要是处理
+    """
+    print("hello world!")
+    args = conf_args()
+    url = args.url
+    file_name = args.file
+    print(f"url = [{url}]")
+    print(f"file_name = [{file_name}]")
+    get_openapi_json(url=url, file_name=file_name)
+
+
+if __name__ == "__main__":
+    main()
