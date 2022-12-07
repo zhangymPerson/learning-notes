b2fec5000cebf5b84b25c9af79f861ffa9fa154f
zhangyanming <zhangyanmingjiayou@163.com>
2022-12-07 10:21:40 +0800

    add pgsql table doc script

--- programming_language/code-demo/db/pgsql/py_pgsql.py
+++ programming_language/code-demo/db/pgsql/py_pgsql.py
@@ -42,18 +42,85 @@ class DB():
         self.conn.close()
 
 
-def exec_sql(db_conn):
+def exec_sql(db_conn, table_name):
     """
     查数据 获取当前库下的表
     """
     # 游标执行返回的是数量
-    db_conn.execute("select * from table_name;")
+    sql = """
+    select
+        c.relname 表名,
+        cast(obj_description(relfilenode, 'pg_class') as varchar) 名称,
+        a.attname 字段,
+        d.description 字段备注,
+        concat_ws('', t.typname, SUBSTRING(format_type(a.atttypid, a.atttypmod) from '\(.*\)')) as 列类型
+    from
+        pg_class c,
+        pg_attribute a,
+        pg_type t,
+        pg_description d
+    where
+        a.attnum>0
+        and
+        a.attrelid = c.oid
+        and
+        a.atttypid = t.oid
+        and
+        d.objoid = a.attrelid
+        and
+        d.objsubid = a.attnum
+        and
+        c.relname in (
+            select
+            tablename
+            from
+            pg_tables
+            where
+            schemaname = 'public'
+            and
+                position('_2' in tablename)= 0
+            and
+                tablename = '%s'
+        )
+    order by
+        c.relname,
+        a.attnum;
+    """
+    db_conn.execute(sql % (table_name))
     # 获取数据库名
-    print(db_conn.name)
     # 需要调用 fetchall() 函数获取结果
     results = db_conn.fetchall()
+    if len(results) < 1:
+        return
+    print(f"- **{table_name} - {results[0][1]}**")
+    print("")
+    print(f"  |字段名|字段类型|字段备注|")
+    print(f"  |---|---|---|")
     for row in results:
-        print("row=[%s]" % (str(row)))
+        print(f"  |{row[2]}|{row[4]}|{row[3]}|")
+    print("")
+
+
+def get_all_table(db_conn):
+    sql = """
+    select
+        tablename
+    from
+        pg_tables
+    where
+        schemaname = 'public'
+        and
+            position('_2' in tablename)= 0
+    """
+    db_conn.execute(sql)
+    # 获取数据库名
+    # 需要调用 fetchall() 函数获取结果
+    results = db_conn.fetchall()
+    list = []
+    for row in results:
+        # print(f"row = [{row}]")
+        list.append(row[0])
+    return list
 
 
 def main():
@@ -64,7 +131,10 @@ def main():
     db_name = "pgsql"
     with DB(host='127.0.0.1', user='root', passwd='123456', db_name=db_name) as db_conn:
         # 获取所有表名
-        exec_sql(db_conn=db_conn)
+        tables = get_all_table(db_conn=db_conn)
+        for table in tables:
+            # print(f"table = [{table}]")
+            exec_sql(db_conn=db_conn, table_name=table)
 
 
 if __name__ == "__main__":
