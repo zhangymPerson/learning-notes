76c0b51107215800b842a9e9fc768d795a9c58ef
zhangyanming <zhangyanmingjiayou@163.com>
2023-02-23 17:31:34 +0800

    alterpython postgresql script

--- programming_language/code-demo/db/pgsql/py_pgsql.py
+++ programming_language/code-demo/db/pgsql/py_pgsql.py
@@ -11,6 +11,12 @@
 
 import psycopg2
 import psycopg2.extras
+import logging
+# 配置日志
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s-%(name)s][%(filename)s:%(lineno)d][%(funcName)s][%(levelname)s]%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
 
 
 class DB():
@@ -27,22 +33,83 @@ class DB():
         self.conn = psycopg2.connect(
             database=db_name, user=user, password=passwd, host=host, port=port)
         # 创建游标，操作设置为字典类型 pgsql 的指定方式  其中 psycopg2.extras 需要单独 import
-        self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        self._cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     def __enter__(self):
-        # 返回游标
-        return self.cur
+        # 返回
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         # 提交数据库并执行
         self.conn.commit()
         # 关闭游标
-        self.cur.close()
+        self._cur.close()
         # 关闭数据库连接
         self.conn.close()
 
+    def get_conn(self):
+        """函数的作用是: 获取数据库连接，删除等操作需要手动提交时使用
+        Args:
+            params:type
+        Returns:
+            return res
+        Raises:
+            列出与接口有关的所有异常.
+        """
+        return self.conn
 
-def exec_sql(db_conn, table_name):
+    def get_cur(self):
+        """函数的作用是: 获取操作数据库的游标
+        Args:
+            params:type
+        Returns:
+            return res
+        Raises:
+            列出与接口有关的所有异常.
+        """
+        return self._cur
+
+
+def create_table(cur, table_name):
+    try:
+        sql = f"""
+drop table IF EXISTS {table_name};
+create TABLE {table_name}(
+    id SERIAL NOT NULL,
+    ip VARCHAR(25),
+    time VARCHAR(255),
+    method VARCHAR(25),
+    url VARCHAR(255),
+    ctime timestamp,
+    mtime timestamp,
+    request jsonb,
+    response jsonb,
+    exception TEXT,
+    remark jsonb,
+    other jsonb,
+    other_msg TEXT,
+    PRIMARY KEY (id)
+);
+comment on table {table_name} is '系统记录表';
+comment on column {table_name}.ip is '请求 ip';
+comment on column {table_name}.time is '耗时';
+comment on column {table_name}.method is '请求方式';
+comment on column {table_name}.url is '请求路由';
+comment on column {table_name}.ctime is '创建时间';
+comment on column {table_name}.request is '请求参数';
+comment on column {table_name}.response is '返回参数';
+comment on column {table_name}.exception is '异常信息';
+comment on column {table_name}.remark is '备注信息';
+comment on column {table_name}.other is '其他扩展';
+comment on column {table_name}.other_msg is '其他扩展字符串';
+        """
+        cur.execute(sql)
+        logging.info(f"建表[{table_name}]成功")
+    except Exception as e:
+        logging.info(f"处理异常,e = [{e}]")
+
+
+def exec_sql(cur, table_name):
     """
     查数据 获取当前库下的表
     """
@@ -86,10 +153,10 @@ def exec_sql(db_conn, table_name):
         c.relname,
         a.attnum;
     """
-    db_conn.execute(sql % (table_name))
+    cur.execute(sql % (table_name))
     # 获取数据库名
     # 需要调用 fetchall() 函数获取结果
-    results = db_conn.fetchall()
+    results = cur.fetchall()
     if len(results) < 1:
         return
     print(f"- **{table_name} - {results[0][1]}**")
@@ -101,7 +168,7 @@ def exec_sql(db_conn, table_name):
     print("")
 
 
-def get_all_table(db_conn):
+def get_all_table(cur):
     sql = """
     select
         tablename
@@ -112,10 +179,10 @@ def get_all_table(db_conn):
         and
             position('_2' in tablename)= 0
     """
-    db_conn.execute(sql)
+    cur.execute(sql)
     # 获取数据库名
     # 需要调用 fetchall() 函数获取结果
-    results = db_conn.fetchall()
+    results = cur.fetchall()
     list = []
     for row in results:
         # print(f"row = [{row}]")
@@ -123,18 +190,33 @@ def get_all_table(db_conn):
     return list
 
 
+def clear_table(table_name, conn):
+    sql = f"delete from {table_name};"
+    try:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(sql)
+        conn.commit()
+        logging.info("删除执行成功")
+    except Exception as e:
+        logging.error(e)
+        conn.rollback()
+
+
 def main():
     """
     主要是处理
     """
     print("hello world!")
-    db_name = "pgsql"
-    with DB(host='127.0.0.1', user='root', passwd='123456', db_name=db_name) as db_conn:
+    db_name = "db_name"
+    table_name = "test"
+    with DB(host='127.0.0.1', user='user', passwd='123456', db_name=db_name) as db:
+        cur = db.get_cur()
+        conn = db.get_conn()
+        create_table(cur, table_name)
         # 获取所有表名
-        tables = get_all_table(db_conn=db_conn)
-        for table in tables:
-            # print(f"table = [{table}]")
-            exec_sql(db_conn=db_conn, table_name=table)
+        # tables = get_all_table(cur=cur)
+        exec_sql(cur=cur, table_name=table_name)
+        clear_table(table_name, conn)
 
 
 if __name__ == "__main__":
