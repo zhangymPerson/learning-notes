91a584b08cb1aca8dd8fa2f223fe9880a6717d5c
zhangyanming <zhangyanmingjiayou@163.com>
2023-08-23 10:14:34 +0800

    add tmux script

--- tools/tmux/work.sh
+++ tools/tmux/work.sh
@@ -1,5 +1,3 @@
-#!/bin/env bash
-
 ###########################################################
 # @file : work.sh
 # @desc : 脚本执行方式 [bash work.sh]
@@ -13,52 +11,113 @@
 # set -x
 
 # set -o errexit 有错误退出
-set -e
+# set -e
 # set -o nounset
-set -u
+# set -u
 # 管道执行过程中有错误退出
-set -o pipefail
+# set -o pipefail
 # 输出脚本中相关内容 到脚本文件名对应的log文件中
 # debug时 可以开启
 # exec >>${0}.log
 
-function create_tmux() {
-    name=$1
-    echo "创建 [$name] 的 tmux 中 ..."
+#######################################
+# 函数的作用是: 后台创建一个 tmux 聊天
+# Globals:
+#   无全局参数说明
+# Arguments:
+#   name 创建的 session name
+# Returns:
+#   返回值为空
+#######################################
+function create_session() {
+    session_name=$1
+    close_session ${session_name}
+    echo "# 创建 [${session_name}] 的 tmux 中 ..."
+    echo "tmux new -s ${session_name} -d"
     # 后台创建会话
-    tmux new -s ${name} -d
-    # 重命名会话的第一个窗口名称为service
-    tmux rename-window -t "${name}:1" service
-    # 切换到指定目录并运行命令
-    tmux send -t "${name}:service" "cd ~/;ls" Enter
+    tmux new -s ${session_name} -d
+    echo ""
+}
 
-    # 默认上下分屏
-    tmux split-window -t "${name}:service"
-    # 切换到指定目录并运行命令
-    tmux send -t "${name}:service" 'cd ~/;date' Enter
+function close_session() {
+    session_name=$1
+    echo "# 关闭 tmux 中的 session[${session_name}]"
+    echo "tmux kill-session -t ${session_name}"
+    tmux kill-session -t ${session_name}
+    echo ""
+}
 
-    # 新建一个名称为tool的窗口
-    # neww等同于new window
-    tmux neww -a -n tool -t ${name}
-    # 运行命令测试
-    tmux send -t "${name}:tool" "cd ~/;echo 'test'" Enter
-    # 水平分屏
-    tmux split-window -h -t "${name}:tool"
-    # 切换到指定目录并执行命令
-    tmux send -t "${name}:tool" "cd ~/;uname" Enter
+function create_window() {
+    session_name=$1
+    window_name=$2
+    echo "# 创建 session[${session_name}] 窗口名[${window_name}]"
+    echo "tmux neww -n ${window_name} -t ${session_name}"
+    tmux neww -n ${window_name} -t ${session_name}
+    echo ""
+}
+
+function split_window() {
+    session_name=$1
+    window_name=$2
+    echo "# session[${session_name}] 窗口名[${window_name}] 上下分屏"
+    echo "tmux split-window -t ${session_name}:${window_name}"
+    tmux split-window -t ${session_name}:${window_name}
+    echo ""
+}
+
+function split_window_h() {
+    session_name=$1
+    window_name=$2
+    echo "# session[${session_name}] 窗口名[${window_name}] 左右分屏"
+    echo "tmux split-window -h -t ${session_name}:${window_name}"
+    tmux split-window -h -t ${session_name}:${window_name}
+    echo ""
+}
+
+function exec_command() {
+    session_name=$1
+    window_name=$2
+    command=$3
+    split_window ${session_name} ${window_name}
+    echo "# session[${session_name}] 窗口名[${window_name}] 执行[${command}]"
+    echo "tmux send -t ${session_name}:${window_name} \"${command}\" Enter"
+    tmux send -t ${session_name}:${window_name} "${command}" Enter
+    echo ""
 }
 
 function list_tmux() {
     echo "创建成功，tmux ls 列表如下:"
     tmux ls
+    echo "使用 [tmux a -t $1]"
+}
+
+function exec_test() {
+    # 传入一个 session 名称 创建
+    session_name=$1
+    create_session ${session_name}
+    window_name="tool"
+    create_window ${session_name} ${window_name}
+
+    command="cd ~"
+    exec_command ${session_name} ${window_name} ${command}
+    exec_command ${session_name} ${window_name} "ls -al"
+
+    window_name="info"
+    create_window ${session_name} ${window_name}
+    command="cd ~"
+    exec_command ${session_name} ${window_name} ${command}
+    exec_command ${session_name} ${window_name} "cd~;echo -e "test""
+
+    window_name="dev"
+    create_window ${session_name} ${window_name}
+    command="cd ~"
+    exec_command ${session_name} ${window_name} ${command}
+    exec_command ${session_name} ${window_name} 'cd /;echo -e "hello world"'
 }
 
 main() {
-    echo "创建一个 tmux session"
-    WORKNAME=$1
-    echo "创建项目 [${WORKNAME}] 的 session"
-    create_tmux ${WORKNAME}
-    list_tmux
+    exec_test $1
+    list_tmux $1
     exit 0
 }
 main "$@"
