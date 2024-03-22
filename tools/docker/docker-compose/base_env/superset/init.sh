e8a3ad31635f117432283c40397326b62d7bfb81
zhangyanming <zhangyanmingjiayou@163.com>
2024-03-22 16:45:35 +0800

    feat: add superset docker compose file

---
+++ tools/docker/docker-compose/base_env/superset/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create admin user
+superset fab create-admin \
+              --username admin \
+              --firstname Superset \
+              --lastname Admin \
+              --email admin@superset.com \
+              --password admin
+            
+# 初始化数据库
+superset db upgrade
+
+# 加载示例数据
+superset load_examples
+
+# superset 初始化
+superset init 
+
+echo "----------------"
+echo "web https://localhost:8080"
+echo "Starting successful"
+bash /usr/bin/run-server.sh
