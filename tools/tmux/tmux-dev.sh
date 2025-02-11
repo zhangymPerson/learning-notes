79959d55bcf6d226657681bda5cdef8f2ea010ea
zhangym <40376181+zhangymPerson@users.noreply.github.com>
2025-02-11 11:27:06 +0800

    Dev (#40)
    * fix(tmux): update split_pane code
    
    * feat(vim): alter vim config
    
    * feat(tools): alter tmux script
    
    * feat(llm/k8s): Add Large Language Model Interaction Tool and Add kubernetes note

--- tools/tmux/tmux-dev.sh
+++ tools/tmux/tmux-dev.sh
@@ -19,6 +19,9 @@ mkdir -p "${PROJECT_PATH}"
 # 判断是否已经创建tmux会话
 if tmux has-session -t "${NAME}"; then
     echo "Tmux session ${NAME} already exists."
+    # 连接到tmux会话
+    tmux attach -t "${NAME}"
+    exit 0
 else
     # 创建 tmux 会话
     tmux new-session -s "${NAME}" -n default -d
@@ -36,8 +39,19 @@ function create_window() {
     tmux send-keys -t "${NAME}:${window_name}" "${command}" C-m
 }
 
+# 分屏
+# 参数1: window_name 窗口名
+function split_pane() {
+    local window_name=$1
+    # 左右分屏
+    # tmux split-window -h -t "${NAME}:${window_name}"
+    # 上下分屏
+    tmux split-window -v -t "${NAME}:${window_name}"
+}
+
 # 创建窗口并执行命令
 create_window code "echo '执行打开项目命令 如: code . 或 vim .'"
+split_pane code
 create_window database "echo '执行数据库连接命令'"
 create_window log "echo '执行日志查看命令'"
 create_window test "echo '执行测试命令'"
